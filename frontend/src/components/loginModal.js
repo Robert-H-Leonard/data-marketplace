3898c42414b5c7a06212b04c784b92f97e199b2c
Park Jack <parker.jackson.hhs@gmail.com>
2022-11-01 22:34:31 -0400

    installed a dependency that streamlines connecting to wallet and add the component to the main page

---
+++ frontend/src/components/loginModal.js
@@ -0,0 +1,34 @@
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+
+const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+    [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+    appName: "My RainbowKit App",
+    chains
+});
+
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider
+});
+
+export default function LoginModal() {
+    return (
+        <WagmiConfig client={wagmiClient}>
+            <RainbowKitProvider chains={chains}>
+                <ConnectButton />
+            </RainbowKitProvider>
+        </WagmiConfig>
+    );
+}
+
