0b5c7af598dbe8af64095688daa7bb96844a019d
Robert Leonard <leonard.robert.h@gmail.com>
2022-11-13 09:09:52 -0500

    Add job button visible if user is logged in

--- frontend/src/components/FilterInput.js
+++ frontend/src/components/FilterInput.js
@@ -1,50 +1,56 @@
-
+import { useAccount } from "wagmi"
 import FormControl from '@mui/material/FormControl'
 import InputLabel from '@mui/material/InputLabel'
-import { TextField } from '@mui/material'
+import { TextField, Button } from '@mui/material'
 import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import AddIcon from '@mui/icons-material/Add';
 
 
 
 export default function FilterInput() {
-    function handleChange() {
-
-    }
+    const { address, isConnected } = useAccount();
     return (
         <div className='filter_input'>
-
-            <form>
-                <FormControl className='search_input' >
-                    <TextField
-                        id="outlined-helperText"
-                        label="Search"
-                        defaultValue="Name, email, etc..."
-                        fullWidth={true}
-                    />
-                </FormControl>
-                <FormControl className='status_input' >
-                    <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-helper-label"
-                        id="demo-simple-select-helper"
-                        value={10}
-                        label="Status"
-                        fullWidth={true}
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={10}>Open to Bid</MenuItem>
-                        <MenuItem value={20}>Pending</MenuItem>
-                        <MenuItem value={30}>Fufilled</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <img  className="filterIcon" src='filterIcon.svg' alt='filterIcon' />
-
-
-            </form>
-
-
+            <div className='.filter_input_left'>
+                <form>
+                    <FormControl className='search_input' >
+                        <TextField
+                            id="outlined-helperText"
+                            label="Search"
+                            defaultValue="Name, email, etc..."
+                            fullWidth={true}
+                        />
+                    </FormControl>
+                    <FormControl className='status_input' >
+                        <InputLabel id="demo-simple-select-helper-label">Status</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            value={10}
+                            label="Status"
+                            fullWidth={true}
+                            onChange={() => {}}
+                        >
+                            <MenuItem value={10}>Open to Bid</MenuItem>
+                            <MenuItem value={20}>Pending</MenuItem>
+                            <MenuItem value={30}>Fufilled</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <img  className="filterIcon" src='filterIcon.svg' alt='filterIcon' />
+                </form>
+            </div>
+            {
+                address 
+                    ? 
+                        <div className='.filter_input_right'>
+                            <Button color="primary" size="medium" variant="contained" startIcon={<AddIcon/>}>
+                                Add Job
+                            </Button>
+                        </div>
+                    :
+                    <></>
+            }
         </div>
     )
 }
